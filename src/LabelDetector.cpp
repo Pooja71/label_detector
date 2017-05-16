e47149b9591a9db8796a36895320444b5e480f30
Pooja71 <poojaa7191@gmail.com>
2017-05-16 18:07:02 -0400

    Fixed bugs for label detection

--- src/LabelDetector.cpp
+++ src/LabelDetector.cpp
@@ -24,11 +24,11 @@ void LabelDetector::FindContours()
 {
     vector<Vec4i> hierarchy;
     RNG rng(12345);
+      findContours(image_op_, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+    
     vector<Point> approx_curve;
     vector<Moments> mu(contours.size());        //finding image moments
     vector<Point2f> mc(contours.size());        //finding centroid of rectangles using image moments
-
-    findContours(image_op_, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
     Mat final_drawing = Mat::zeros(image_op_.size(), CV_8UC3);  //Image showing the contours
 
@@ -44,15 +44,17 @@ void LabelDetector::FindContours()
        {
             for (int j = 0; j < approx_curve.size(); j++)
                 cout << "Vertices : " << (int)approx_curve[j].x << ',' << (int)approx_curve[j].y << '\n';
-    
             //get moments
             mu[i] = moments(contours[i], false);
 
+ROS_INFO("Line50");
             //get mass centers
             mc[i] = Point2f((float)(mu[i].m10 / (mu[i].m00)) , (float)(mu[i].m01 / (mu[i].m00)));
             cv::Point2f location = Point2f(mc[i].x / image_ip_.cols, mc[i].y/image_ip_.rows);
+ROS_INFO("Line54");
             Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
             drawContours(final_drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
+ROS_INFO("Line57");
             cout << "Contour Area : " << contourArea(contours[i]) << '\t' << "Contour Perimeter : " << arcLength(contours[i], true) << '\t' << "Location : " << location << '\n';
             string text = "Area-" + std::to_string(contourArea(contours[i])) + " Perimeter-" + std::to_string(arcLength(contours[i], true)) + '\n'; // +"Centroid-" + std::to_string(mc[i]);
             putText(final_drawing, text, mc[i], FONT_HERSHEY_COMPLEX_SMALL, 0.5, cvScalar(200, 200, 250), 1, CV_AA);
